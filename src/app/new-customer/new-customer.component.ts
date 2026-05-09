e122dceff1e362d91231775155ba4a3b192bb87b
HermesThemes <momoboogeyman2000@gmail.com>
2026-05-09 18:06:17 +0000

    components

--- src/app/new-customer/new-customer.component.ts
+++ src/app/new-customer/new-customer.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Customer} from "../model/customer.model";
+import {CustomerService} from "../services/customer.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-new-customer',
-  standalone: true,
-  imports: [],
   templateUrl: './new-customer.component.html',
-  styleUrl: './new-customer.component.css'
+  styleUrls: ['./new-customer.component.css'],
+  standalone: true,
+  imports: [ReactiveFormsModule]
 })
-export class NewCustomerComponent {
+export class NewCustomerComponent implements OnInit {
+  newCustomerFormGroup! : FormGroup;
+  constructor(private fb : FormBuilder, private customerService:CustomerService, private router:Router) { }
 
+  ngOnInit(): void {
+    this.newCustomerFormGroup=this.fb.group({
+      name : this.fb.control(null, [Validators.required, Validators.minLength(4)]),
+      email : this.fb.control(null,[Validators.required, Validators.email])
+    });
+  }
+
+  handleSaveCustomer() {
+    let customer:Customer=this.newCustomerFormGroup.value;
+    this.customerService.saveCustomer(customer).subscribe({
+      next : data=>{
+        alert("Customer has been successfully saved!");
+        //this.newCustomerFormGroup.reset();
+        this.router.navigateByUrl("/customers");
+      },
+      error : err => {
+        console.log(err);
+      }
+    });
+  }
 }
